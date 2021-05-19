8c0665118a3c62bd712d762d3afa3dcf2a1b14f6
geniusLcy <im.lichenyi@gmail.com>
2021-05-19 23:30:55 +0800

    update RDT stop and wait: TIMER=100.0

--- RDT_stopAndWait_ver.c
+++ RDT_stopAndWait_ver.c
@@ -58,7 +58,7 @@ int B_status;
 /* universal global utils */
 #define MODNUM 19260817     /* for checksum */
 #define BASENUM 114514      /* for checksum */
-#define MAXTIME 100.0
+#define MAXTIME 100.0       /* for timer */
 #define DEBUG
 
 /* help functions */
